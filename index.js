49ce85196ccbcae0842d32c80012b3f3c600fc81
nick-gk <nicolae.gincota@gmail.com>
2020-12-13 15:56:09 +0200

    change / get message

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use("/api/auth", require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send("API Running"));
+app.get('/', (req, res) => res.send("BugTracker is online"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
