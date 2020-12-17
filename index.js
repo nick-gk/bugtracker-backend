720f10632f9b32a1e2b95fab6f61714e6d95f8a0
alexandragherghel <alexandraioana.gherghel@gmail.com>
2020-12-17 17:30:41 +0200

    Sper sa ajute macar putin ce am scris

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ connectDB();
 app.use(express.json({extended: false}));
 
 app.use("/api/auth", require('./routes/api/auth'));
-
+app.use("/api/project", require("./routes/api/project"));
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send("BugTracker Ale is online"));
