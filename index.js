7ddd5c81b7dcc3d60fc2278ef3d4424d9171adf9
Nicolae Gincota <nicolae.gincota@cst.ro>
2020-12-19 01:12:59 +0200

    readme file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -24,6 +25,6 @@ app.use("/api/auth", require('./routes/api/auth'));
 app.use("/api/project", require("./routes/api/project"));
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send("BugTracker 1082 is online"));
+app.get('/', (req, res) => res.sendFile((path.join(__dirname + '/README.md'))));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
